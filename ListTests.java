800de75627ab458022b8c937490e61f3aecf0670
Girish Krishnan <gikrishnan@ucsd.edu>
2023-10-23 16:50:47 -0700

    Fix bug in listExamples

--- ListTests.java
+++ ListTests.java
@@ -21,4 +21,27 @@ public class ListTests {
     }));
   }
 
+  @Test
+  public void testMerge () {
+    List<String> input1 = new ArrayList<>();
+    input1.add("a");
+    input1.add("b");
+    input1.add("e");
+
+    List<String> input2 = new ArrayList<>();
+    input2.add("c");
+    input2.add("d");
+    input2.add("f");
+
+    List<String> expected1 = new ArrayList<>();
+    expected1.add("a");
+    expected1.add("b");
+    expected1.add("c");
+    expected1.add("d");
+    expected1.add("e");
+    expected1.add("f");
+
+    assertEquals(expected1, ListExamples.merge(input1, input2));
+  }
+
 }
